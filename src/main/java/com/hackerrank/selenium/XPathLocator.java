f0cd074fa5e7a31389ccc05cf5ee4b09a72bee26
Git Server Admin <admin@git-server>
2022-01-12 07:46:47 +0000

    Add initial repository

---
+++ src/main/java/com/hackerrank/selenium/XPathLocator.java
@@ -0,0 +1,16 @@
+package com.hackerrank.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class XPathLocator {
+
+    public static String relativeXPathForInput(WebDriver driver, String pageUrl) {
+        //return xpath
+        return null;
+    }
+
+    public static String absoluteXPathForInput(WebDriver driver, String pageUrl) {
+        //return xpath
+        return null;
+    }
+}
